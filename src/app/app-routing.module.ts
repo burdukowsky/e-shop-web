d5e60f6aa14bd16e4bd337d1dd78d424ef258ad9
Stanislav Burdukowsky <burdukowskystas@gmail.com>
2018-05-29 20:01:00 +0700

    Create pages and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {ProductsComponent} from './products/products.component';
+import {ProductComponent} from './products/product/product.component';
+import {AboutComponent} from './about/about.component';
+import {CartComponent} from './cart/cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'products'},
+  {path: 'products', component: ProductsComponent},
+  {path: 'products/:id', component: ProductComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'cart', component: CartComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductComponent } from './products/product/product.component';
+import { CartComponent } from './cart/cart.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProductsComponent,
+    ProductComponent,
+    CartComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
